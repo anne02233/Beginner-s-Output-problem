91c3134518b7586b61ea0fcc23629d62e3b79337
Annieeeee <147971245+anne02233@users.noreply.github.com>
2024-05-01 16:27:09 +1200

    beginners starter 2.py

---
+++ beginners 2.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+adjective = input("Enter an adjective (describing word): ")
+verb = input("Enter a verb: ")
+place = input("Enter a place: ")
+food = input("Enter a food: ")
+vehicle = input("Enter a vehicle: ")
+
+mad_lib = f"{name} is a very {adjective} person to be around. They love to {verb} in {place}. Their favourite food is {food} which they eat in a {vehicle}."
+print(mad_lib)
